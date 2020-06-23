055d2860ffa393ccd4a73fba1b195e3c675380db
Aditio Dharma Putra <Aditiod@xl.co.id>
2020-06-23 23:44:50 +0700

    Add webservices

---
+++ NewsAPI-SwiftUI/Services/WebServices.swift
@@ -0,0 +1,13 @@
+//
+//  WebServices.swift
+//  NewsAPI-SwiftUI
+//
+//  Created by Aditio Dharma Putra on 23/06/20.
+//  Copyright © 2020 Aditio Dharma Putra. All rights reserved.
+//
+
+import Foundation
+
+class WebServices {
+    
+}
