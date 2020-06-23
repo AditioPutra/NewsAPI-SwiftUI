4e3854ed05d7271622042d9db66fa0ad323bff4b
Aditio Dharma Putra <Aditiod@xl.co.id>
2020-06-23 23:47:03 +0700

    Applied MVVM Pattern

---
+++ NewsAPI-SwiftUI/Models/TopHeadLine.swift
@@ -0,0 +1,9 @@
+//
+//  TopHeadLine.swift
+//  NewsAPI-SwiftUI
+//
+//  Created by Aditio Dharma Putra on 23/06/20.
+//  Copyright © 2020 Aditio Dharma Putra. All rights reserved.
+//
+
+import Foundation
